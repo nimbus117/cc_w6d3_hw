db8718a97b2d71eb9682a89d57434a683a99cc67
nimbus117 <mail@nimbus117.co.uk>
2018-09-05 16:15:54 +0100

    Isogram passing

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,11 @@
 const IsogramFinder = function (word) {
-
+  this.word = word.split('');
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
-}
+  return this.word.every((letter, index, array) => {
+    return array.filter(x => x === letter).length <= 1;
+  })
+};
 
 module.exports = IsogramFinder;
