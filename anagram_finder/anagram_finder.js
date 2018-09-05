c2b6c42189df041efd895c005f0928d7748f2172
nimbus117 <mail@nimbus117.co.uk>
2018-09-05 17:01:38 +0100

    passed 'should not detect a word as it's own anagram, regardless of case' test

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -4,7 +4,7 @@ const AnagramFinder = function (word) {
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
   let result = otherWords.filter(word => word.length === this.word.length);
-  result = result.filter(word => word !== this.word);
+  result = result.filter(word => word.toLowerCase() !== this.word);
   return result.filter(word => {
     return word.toLowerCase().split('').every(letter => {
       return this.word.includes(letter);
