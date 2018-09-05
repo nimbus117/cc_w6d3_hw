6f72aff246253f885217f84cc2b9363c4092e6be
nimbus117 <mail@nimbus117.co.uk>
2018-09-05 16:48:16 +0100

    Anagram passing

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,16 @@
 const AnagramFinder = function (word) {
-
+  this.word = word.toLowerCase();
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+  let result = otherWords.filter(word => word.length === this.word.length);
+  result = result.filter(word => word !== this.word);
+  result = result.filter(word => {
+    return word.toLowerCase().split('').every(letter => {
+      return this.word.toLowerCase().includes(letter);
+    })
+  })
+  return result;
 }
 
 module.exports = AnagramFinder;
